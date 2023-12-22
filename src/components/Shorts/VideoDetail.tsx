273c0040fcba86830cc9833c561a37a9077edd77
KUcodemaster <jh9485@korea.ac.kr>
2023-12-22 17:53:23 +0900

    gogogo

--- src/components/Shorts/VideoDetail.tsx
+++ src/components/Shorts/VideoDetail.tsx
@@ -1,4 +1,4 @@
-import React, { LegacyRef, useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Video } from "../../types/video";
@@ -10,25 +10,23 @@ interface VideoProps {
 
 export const VideoDetail = ({ videos }: VideoProps) => {
   const navigate = useNavigate();
-
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [inViewRef, inView] = useInView({
-    triggerOnce: true, // 한 번만 트리거
-    threshold: 0.2, // 화면에 20% 이상 노출되면 트리거
-  });
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [ref, inView] = useInView();
 
   useEffect(() => {
-    if (inView && videoRef.current) {
-      videoRef.current.play(); // 화면에 나타날 때 비디오 재생
-    } else {
-      videoRef.current?.pause(); // 화면 밖으로 나갈 때 비디오 일시 중지
+    if (videoRef.current) {
+      if (inView) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
     }
   }, [inView]);
 
   return (
     <VideoDetailContainer>
       {videos.map((video) => (
-        <VideoCard ref={inViewRef} key={video.videoId}>
+        <VideoCard ref={ref} key={video.videoId}>
           <video ref={videoRef} controls>
             <source src={video.videoUrl} type="video/mp4" />
           </video>
